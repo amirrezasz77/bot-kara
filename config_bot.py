2be076b750fe740e27ba4e2084ec236c5be083a9
Amirrezasz77 <95874402+amirrezasz77@users.noreply.github.com>
2025-05-02 19:55:48 +0330

    Create config_bot.py

---
+++ config_bot.py
@@ -0,0 +1,52 @@
+db = 'b2'
+# - - - - - - - - - - - - - #
+soundCloudKey = 'ee61fbdb48ee35377e1847f4e2af152d'
+# - - - - - - - - - - - - - #
+youtubeApi = "AIzaSyAaj_x-9QyekMxmELDoS5C7oFRDdXPnFys" # https://console.developers.google.com/ | #pishnahad
+# - - - - - - - - - - - - - #
+telegram_datas = {
+"botToken": "7540724852:AAHAecJjigTAt4WCcFlx0Bc2W_b1eQxv3vY", # https://t.me/botFather
+"api_hash": "e182ccdf00360ac60052399b211fc8d7", # https://my.telegram.org/auth
+"api_id": 652083, # https://my.telegram.org/auth
+"device_model": "Linux",
+"system_version": "Ubuntu 18.04",
+"app_version": "1.0",
+}
+# - - - - - - - - - - - - - #
+sudo_users = (
+777000,
+349866712.5//2.5,
+telegram_datas['botToken'].split(':')[0],
+# Put down
+7540724852, #optional
+)
+# - - - - - - - - - - - - - #
+instagram_datas = {
+"username": "amirreza_w_d",
+"password": "Rez1377!", 
+} # https://instagram.com | #hatman
+# - - - - - - - - - - - - - #
+IDs_datas = {
+"sudo_id": 5402738327,
+"bot_id": int(telegram_datas['botToken'].split(':')[0]),
+"chUsername": "amirreza_W_D",
+"chLink": "https://t.me/+WMKxVZPxvOA5ODhi",
+}
+# - - - - - - - - - - - - - #
+server_datas = {
+"ip": "easy-crabs-kick.loca.lt",
+"port_server": 10250, #optional | #hatman
+"port_tg": 443, #80, 88, 443, 8443 | #hatman
+}
+# - - - - - - - - - - - - - #
+acr_datas = {
+'host': "identify-eu-west-1.acrcloud.com",
+'access_key': "986f5a86eaf372e061b87630b17d5f94",
+'access_secret': "ZNKMNjHsG2J6K0NM8bgLBrBSCxmgKf7sX6Uh2IVf",
+'timeout': 10,
+} # https://eu-console.acrcloud.com/account/keys | #pishnahad
+# - - - - - - - - - - - - - #
+deezer_token = "34f33f99556f83790f92e5d0dc84bdd831c23a50b3972c7aa4306f18548ae017f7cc6791f99559c33f6c0433f20f2a50a45b04b15d04b35bcae6060deebb56302313ed08fb0f93f89be672dd54ce7b48560aa1953b38af55854600452a92fdd9" # https://www.deezer.com/ | home page account | #hatman
+# - - - - - - - - - - - - - #
+sendApi = "https://api.telegram.org/bot{}/".format(telegram_datas['botToken'])
+# - - - - - - - - - - - - - #
